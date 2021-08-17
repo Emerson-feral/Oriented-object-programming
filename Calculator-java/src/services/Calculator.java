7f822b11eb976c944e3a63693c0ec726fc0953af
Emerson Fernandes <emerson_feral@hotmail.com>
2021-08-17 20:56:11 +0200

    Calculator class created

---
+++ Calculator-java/src/services/Calculator.java
@@ -0,0 +1,18 @@
+package services;
+
+public class Calculator {
+    
+    public Integer addition(Integer value1 , Integer value2) {
+        return value1 + value2;
+    }
+    public Integer subtraction(Integer value1 , Integer value2) {
+        return value1 - value2;
+    }
+    public Integer multiplication (Integer value1 , Integer value2) {
+        return value1 * value2;
+    }
+    public Integer division(Integer value1 , Integer value2) {
+        return value1 / value2;
+    }
+
+}
